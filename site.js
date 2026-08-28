cb792cfa7714a892fa04460fe86c58e877d0fd9a
Arkabama <matthewjbradley.77@gmail.com>
2026-08-27 18:03:36 -0700

    Finalize FRSCI header and Hall of Fame polish

--- site.js
+++ site.js
@@ -2,10 +2,6 @@
   const toggle = document.querySelector('.menu-toggle');
   const nav = document.querySelector('.site-nav');
 
-  document.querySelectorAll('.brand-mark img').forEach((img) => {
-    img.src = '/frsci-logo-header.png';
-  });
-
   if (toggle && nav) {
     toggle.addEventListener('click', () => {
       const open = nav.classList.toggle('is-open');
